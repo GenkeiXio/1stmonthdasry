922c02c7b5959ccc07ce906a43a5577c149d6651
Prince Louis M. Jaylo <jayloprincelouis123@gmail.com>
2025-03-16 16:39:15 +0800

    love

---
+++ script.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Bianca My love",
+
+    background: "background.jpg",
+
+    min1: "min1.jpeg",
+    min2: "min2.jpeg",
+    min3: "min3.jpeg",
+    min4: "min4.jpeg",
+    min5: "min5.jpeg",
+    min6: "min6.jpeg",
+
+    max1: "max1.jpeg",
+    max2: "max2.jpeg",
+    max3: "max3.jpeg",
+    max4: "max4.png",
+    max5: "max5.jpeg",
+    max6: "max6.jpeg", 
+}
