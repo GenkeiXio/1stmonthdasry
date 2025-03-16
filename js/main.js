62423fd935380432be91667b6a98cb34f31acf84
Prince Louis M. Jaylo <jayloprincelouis123@gmail.com>
2025-03-16 16:53:35 +0800

    love

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ $(document).ready(function() {
     for (let i = 1; i <= 6; i++)
         $('#max' + i).css('background-image', 'url(./images/' + CONFIG['max' + i] + ')')
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const bgMusic = document.getElementById("bg-music");
+    if (sessionStorage.getItem("musicPlaying")) {
+        bgMusic.play();
+    }
+    // Try playing the music automatically
+    const playMusic = () => {
+        bgMusic.play().catch(() => {
+            console.log("Autoplay prevented, waiting for user interaction.");
+        });
+    };
+
+    // Play music on first user interaction
+    document.body.addEventListener("click", playMusic, { once: true });
+});
